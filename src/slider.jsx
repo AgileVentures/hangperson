c7b57512395a619c731859fdb9f2fb3f5d5defe3
marianmosley <marian.mosley@gmail.com>
2016-06-19 15:45:02 -0400

    Add default value and on change event handler to slider

--- src/slider.jsx
+++ src/slider.jsx
@@ -1,8 +1,15 @@
 var React = require("React");
 module.exports = React.createClass({displayName: 'Slider',
+	getInitialState: function() {
+    return {value: '10'};
+  },
+  handleChange: function(event) {
+    this.setState({value: event.target.value});
+    console.log(event.target.value);
+  },
   render: function() {
     return (
-      <input type="range" min="0" max="50" value="25" step="5"></input>
+      <input type="range" min="0" max="50" name="points" defaultValue="10" step="5" onChange={this.handleChange}></input>
     );
   }
 });
