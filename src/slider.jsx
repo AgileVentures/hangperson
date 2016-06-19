6d4bc086f16c619405f03c1d45ae2f76372744da
mtc2013 <mtc2013opensource@gmail.com>
2016-06-19 16:36:52 -0400

    Add message and some packages"

--- src/slider.jsx
+++ src/slider.jsx
@@ -8,8 +8,12 @@ module.exports = React.createClass({displayName: 'Slider',
     console.log(event.target.value);
   },
   render: function() {
+    const text = this.state.value+" "+ "pieces of gold!";
     return (
+      <div>
+      <h5>{text}</h5>
       <input type="range" min="0" max="50" name="points" defaultValue="10" step="5" onChange={this.handleChange}></input>
+      </div>   
     );
   }
 });
