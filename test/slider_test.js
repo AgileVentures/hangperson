ffc0c2ed3a5b53196dcd8209e24f73a028ff851e
mtc2013 <mtc2013opensource@gmail.com>
2016-06-21 18:16:52 -0400

    Test moving of slider updates text

--- test/slider_test.js
+++ test/slider_test.js
@@ -8,4 +8,9 @@ describe("slider", function() {
   it("defaults to 10 pieces of gold", function() {
     expect(shallow(<Slider />).contains(<GoldTagLine goldPieces="10"/>)).to.be.true;
   });
+  it("changes description when slider is moved", function(){
+    const slider = mount(<Slider />);
+    slider.find("input").simulate('change', {target: {value: 15}})
+    expect(slider.contains(<GoldTagLine goldPieces="15"/>)).to.be.true;
+  });
 });
