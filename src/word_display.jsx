2891a2d29352931efd77c37671e53a7fae4c5fd1
mtc2013 <mtc2013opensource@gmail.com>
2016-07-02 18:55:43 -0400

    Got to run

--- src/word_display.jsx
+++ src/word_display.jsx
@@ -8,7 +8,24 @@ class WordDisplay extends React.Component {
   constructor(props) {
     super(props);
   }
+  
   render() {
-    return()
+  	var underscores = [];
+  	for(var i = 0; i < this.props.wordLength; i++){
+  		if(i === 0){
+  			underscores.push("_");
+  		}else{
+  	 		underscores.push(" _");
+  		}
+   	}
+    return(
+      <div>{underscores}</div>
+    );
   }
 }
+
+
+WordDisplay.propTypes = propTypes;
+WordDisplay.defaultProps = defaultProps;
+
+export default WordDisplay;
--- test/word_display_test.js
+++ test/word_display_test.js
@@ -5,7 +5,12 @@ import {expect} from 'chai';
 
 describe("word_display", function() {
   it("displays 6 underscores when initialized with a 6 letter word and 0 correct guesses", function() {
-    const word_display = shallow(<WordDisplay />);
+    const word_display = shallow(<WordDisplay wordLength="6"/>);
+    expect(word_display.text()).to.equal("_ _ _ _ _ _");
+  });
+  it("displays 8 underscores when initialized with a 8 letter word and 0 correct guesses", function() {
+    const word_display = shallow(<WordDisplay wordLength="8"/>);
+    expect(word_display.text()).to.equal("_ _ _ _ _ _ _ _");
   });
 });
 
